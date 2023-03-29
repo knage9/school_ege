89066a560d3045efac765f7fe1e82dbfe43fab43
knage9 <knagh.forall@gmail.com>
2023-03-29 09:58:27 +0300

    itertools

--- school/ЕГЭ/ege/12_3.py
+++ school/ЕГЭ/ege/12_3.py
@@ -1,7 +1,7 @@
 s = "1" * 100
 while "111" in s or "88888" in s:
     if "111" in s:
-        s.replace("111", "88", 1)
+        s = s.replace("111", "88", 1)
     else:
-        s.replace("88888", "8", 1)
+        s = s.replace("88888", "8", 1)
 print(s)
